ca54679defe52fa7c7f886801288eedfcd85dedc
janitharasanjana <janitharasanjana59@gmail.com>
2025-03-23 17:44:08 +0530

    change code lines

--- car_rental_megaCity-FE--main/src/components/Home/Home.jsx
+++ car_rental_megaCity-FE--main/src/components/Home/Home.jsx
@@ -4,12 +4,7 @@ import image5 from '../../assets/images/7197355.jpg';
 
 const Home = () => {
 
-    // const backgroundStyle = {
-    //     backgroundImage: 'url(/images/7197355.jpg)',  // Using relative path from public folder
-    //     backgroundSize: 'cover',
-    //     backgroundPosition: 'center',
-    //     height: '100vh', // Ensures it takes full height of the viewport
-    // };
+    
 
     return (
         <div className="container mt-5" style={{  backgroundImage: `url(${image5})`, backgroundSize: 'cover', backgroundPosition:'center', height: '80vh',  textAlign: 'center', display: 'flex',justifyContent: 'center' }}>
