ee1cc6aa1bdb0a810725a93a0e234af207685bf5
janitharasanjana <janitharasanjana59@gmail.com>
2025-03-23 17:27:57 +0530

    Resolved merge conflicts

---
+++ car_rental_megaCity-FE--main/src/components/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+//import backgroundImage from '../../assets/images/7197355.png';
+import image5 from '../../assets/images/7197355.jpg';
+
+const Home = () => {
+
+    // const backgroundStyle = {
+    //     backgroundImage: 'url(/images/7197355.jpg)',  // Using relative path from public folder
+    //     backgroundSize: 'cover',
+    //     backgroundPosition: 'center',
+    //     height: '100vh', // Ensures it takes full height of the viewport
+    // };
+
+    return (
+        <div className="container mt-5" style={{  backgroundImage: `url(${image5})`, backgroundSize: 'cover', backgroundPosition:'center', height: '80vh',  textAlign: 'center', display: 'flex',justifyContent: 'center' }}>
+            <h1>Welcome!</h1>
+            {/* <p>This is the home page. You can proceed with making reservations here.</p> */}
+        </div>
+    );
+
+}
+
+export default Home;
